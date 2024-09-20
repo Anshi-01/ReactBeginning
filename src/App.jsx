ba2dfa8f9f7abac927b3c352027f1f5f7a0cd6c7
Anshi-01 <anshisrivastav16@gmail.com>
2024-09-20 20:25:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //javascript logic
+
+  return(
+    <div >
+      <h1>Vite React Project</h1>
+      
+      <p>This is a Vite project using the React framework.</p>
+      <p>By Anshika Srivastava.</p>
+    </div>
+  )
+
+  
+}
+
+export default App
